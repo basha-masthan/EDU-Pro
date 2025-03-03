1d7ae3260a74e4bdf217d2785580bf1897521dcd
basha-masthan <bashamasthan31@gmail.com>
2025-03-03 23:44:57 +0530

    Vercel

--- proj1/wsgi.py
+++ proj1/wsgi.py
@@ -9,9 +9,9 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-# from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vercel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proj1.settings')
 
-# application = get_wsgi_application()
-# app = application
+application = get_wsgi_application()
+app = application
